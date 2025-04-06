b5e092b6d6d5dc1257bf936453d460ce1b07417c
Teranovas <125176569+Teranovas@users.noreply.github.com>
2025-04-06 17:29:52 +0900

    업그레이드

--- app/src/main/java/com/example/studywords/MainActivity.kt
+++ app/src/main/java/com/example/studywords/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.studywords
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.cardview.widget.CardView
+import com.example.studywords.community.BoardActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var communityBtn : CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        communityBtn = findViewById(R.id.communityCard)
+        communityBtn.setOnClickListener(View.OnClickListener {
+
+            val intent = Intent(this, BoardActivity::class.java)
+            startActivity(intent)
+
+        })
+
+
     }
 }
---
+++ app/src/main/java/com/example/studywords/community/BoardActivity.kt
@@ -0,0 +1,13 @@
+package com.example.studywords.community
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+class BoardActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
+
+
